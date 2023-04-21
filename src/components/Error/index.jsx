18fa3616adfaf79b8a61742ae2503b866ef0d1be
Fernando Soto <fernandosotto5888@gmail.com>
2023-04-21 08:33:21 -0400

    actualizando

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,8 +1,12 @@
+import { Helmet } from "react-helmet"
 import { Link } from "wouter"
 
 export const Error = ({error})=> {
     return (
         <section className="error">
+            <Helmet>
+                <title>Car Rental | Error</title>
+            </Helmet>
             <main className="error__main">
             <h1 className="error__h1">{error}</h1>
             <Link href="/" className="error__a">Volver al inicio</Link>
